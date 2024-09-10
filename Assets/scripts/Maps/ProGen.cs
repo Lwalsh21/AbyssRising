035b18a2995c7a808531418af95db41ec6322fb6
Lwalsh21 <97565562+Lwalsh21@users.noreply.github.com>
2024-09-10 01:55:30 -0400

    maps

---
+++ Assets/scripts/Maps/ProGen.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+sealed class ProGen : MonoBehaviour
+{
+	public void GenerateDungeon(int mapWidth, int mapHeight, int roomMinSize, int MaxRooms, List<RectangularRoom> rooms)
+	{
+		
+	}
+}
